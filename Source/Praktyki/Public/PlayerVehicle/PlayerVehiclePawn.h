d63094c24ff84e2d6507f7e7899ac034ea75c35b
Jakub Paluch <paluchk@gmail.com>
2025-05-26 16:24:16 +0200

    Created a vehicle class.

---
+++ Source/Praktyki/Public/PlayerVehicle/PlayerVehiclePawn.h
@@ -0,0 +1,17 @@
+// Copyright 2025 Teyon. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehiclePawn.h"
+#include "PlayerVehiclePawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRAKTYKI_API APlayerVehiclePawn : public AWheeledVehiclePawn
+{
+	GENERATED_BODY()
+	
+};
